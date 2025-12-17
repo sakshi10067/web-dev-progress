38015bf8186e1d8c1d32b0f5bb630e910c0e6fc1
sakshi10067 <sakshiisingh0@gmail.com>
2025-12-17 23:58:01 +0530

    DOM

---
+++ DOM&BOM/children.js
@@ -0,0 +1,10 @@
+// document.body.firstChild
+// #text
+// document.body.childNodes[1] in console
+
+// document.body.firstElementChild
+// <div class=​"container">​…​</div>​
+// document.body.firstElementChild.childNodes
+// NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+// document.body.firstElementChild.children
+// HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
