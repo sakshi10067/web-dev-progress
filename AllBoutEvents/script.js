2a20992893b5259a80ac770efd495bdd500935b4
sakshi10067 <sakshiisingh0@gmail.com>
2026-03-21 21:45:38 +0530

    events in js

---
+++ AllBoutEvents/script.js
@@ -0,0 +1,9 @@
+// let button=document.getElementById("btn");
+// button.addEventListener("click",()=>{
+//     // alert("you clicked the button bro");
+//     document.querySelector(".container").innerHTML+="<p>you clicked the button bro</p>";
+// })
+
+document.addEventListener("keydown",(e)=>{
+    console.log(e);
+})
